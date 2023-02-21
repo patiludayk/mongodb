d48d76fcafea2033c117c01ff17b9346df7f94a9
patiludayk <patiludayk@gmail.com>
2023-02-21 23:15:31 +0000

    database url to properties file

--- src/main/java/com/uday/mongodb/config/MongoConfig.java
+++ src/main/java/com/uday/mongodb/config/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -17,14 +18,21 @@ import java.util.Collections;
 @Configuration
 @EnableMongoRepositories(basePackages = "com.uday.mongodb.repository")
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${database.url}")
+    private String databaseUrl;
+
+    @Value("${databaseName:userdb}")
+    private String databaseName;
+
     @Override
     protected String getDatabaseName() {
-        return "userdb";
+        return databaseName;
     }
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/userdb");
+        ConnectionString connectionString = new ConnectionString(databaseUrl);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
 
         return MongoClients.create(mongoClientSettings);
